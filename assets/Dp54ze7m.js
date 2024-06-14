9c49bff5bd282aa54c90653838c95e3e2bab31fe
Puffindor <108236505+Puffindor@users.noreply.github.com>
2024-06-14 10:04:49 +0000

    Deploying to gh-pages from @ Puffindor/opt6@09361982ac55e48e3e27e80c03c2522228a4ecc9 🚀

---
+++ assets/Dp54ze7m.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t}from"./QAWyaKE-.js";const n={};function o(r,s){return t(),c("h1",null,"Регистрация")}const _=e(n,[["render",o]]);export{_ as default};
--- assets/f7-jvWrv.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,c as e,o as t}from"./DW2kZiuV.js";const n={};function o(r,s){return t(),e("h1",null,"Контакты")}const _=c(n,[["render",o]]);export{_ as default};
